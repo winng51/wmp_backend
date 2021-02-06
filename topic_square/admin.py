aefad6289bd5f1e6da26bfd6c511ee6bc276e5ce
winng51 <houtianhong502@163.com>
2021-02-06 20:13:24 +0800

    增加图片类，关联标签和话题、话题和图片，修改对应接口

--- topic_square/admin.py
+++ topic_square/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Label, Topic
+from .models import Label, Topic, Picture
 
 # Register your models here.
 
@@ -12,10 +12,25 @@ class LabelAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'pub_user', 'pub_time']
 
 
+class PictureInline(admin.TabularInline):
+    model = Picture
+    extra = 1
+
+
 class TopicAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'user_name', 'create_time']
-    list_filter = ('is_homework', 'labels_test')
-    filter_horizontal = ('labels_test',)
+    list_filter = ('is_homework', 'labels')
+    filter_horizontal = ('labels',)
+    fieldsets = (
+        ['正文内容', {
+            'fields': ('labels', 'title', 'user_name', 'user_id', 'avatar',
+                       'content', 'is_homework')}],
+        ['互动内容', {
+            'fields': (('comment_count', 'comments'),
+                       ('star_count', 'stars'),
+                       ('view_count', 'views'))}]
+    )
+    inlines = [PictureInline]
 
 
 admin.site.register(Label, LabelAdmin)
